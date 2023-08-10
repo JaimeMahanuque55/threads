eb50a2bf7f597f8e1901480deaebf035f8efe362
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2023-08-10 08:44:13 -0400

    isActive variable config

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -14,6 +14,7 @@ function LeftSidebar() {
       <section className="custome-scrollbar leftsidebar">
         <div className="flex w-full flex-1 flex-col gap-6 px-6">
           {sidebarLinks.map((link) => {
+            const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
           
           return (
             <div>
