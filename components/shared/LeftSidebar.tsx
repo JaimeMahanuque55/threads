11a914d3ee37cfe1ea9609565446b712c1ef71f7
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2023-08-10 08:46:54 -0400

    isActive logic

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -21,7 +21,7 @@ function LeftSidebar() {
               <Link 
                 href={link.route}
                 key={link.label}
-                className="leftsidebar_link"
+                className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
                 >
                 <Image 
                   src={link.imgURL}
